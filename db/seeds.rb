2dcf02a0d9e4eb14c87847bca74619889cd5bf8a
Paul Lee <paul.lee@cantab.net>
2017-10-12 16:13:48 +0900

    rails cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+puts 'Creating seeds...'
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Sangria")
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "red wine")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "bacardi")
+Ingredient.create(name: "cranberries")
+Ingredient.create(name: "lime juice")
+Ingredient.create(name: "water")
+
+puts 'Finished!'
